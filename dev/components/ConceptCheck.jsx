1969cf1dab009455fd81d44f73b8f744fde8ba0e
Bryan Mehall <bryanmehall@gmail.com>
2017-06-19 16:57:53 -0700

    move icons to styles.jsx

--- dev/components/ConceptCheck.jsx
+++ dev/components/ConceptCheck.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {colors} from './styles'
+import {colors, icons} from './styles'
 
 class ConceptCheck extends React.Component {
 
@@ -9,43 +9,13 @@ class ConceptCheck extends React.Component {
 		const onAsk = this.props.onAsk
 		const questions = this.props.questions
 		const showQuestions = true
+
 		const textStyle = {
 			cursor: "default",
 			MozUserSelect: "none",
 			WebkitUserSelect: "none",
 			msUserSelect: "none",
 		}
-
-		const icons = {
-			completed: (
-				<g transform = "translate(20,20) scale(2)">
-					<path d="M-5,0 L-2,3 L5,-4" strokeWidth={2.5} fill="none" stroke="rgb(88, 222, 88)"></path>
-				</g>
-			),
-
-			active: (
-				<g transform = "translate(20,20) scale(1.1)">
-					<circle cx={0} cy={0} r={10} fill="rgb(47, 47, 244)"/>
-					<text
-						x={0}
-						y={1}
-						fill="#fff"
-						fontSize= {15}
-						textAnchor= "middle"
-						alignmentBaseline="middle"
-						style={textStyle}
-						>?</text>
-				</g>
-			),
-
-			inactive: (
-				<g transform="translate (20,20)">
-					<circle cx={0} cy={0} r={10} stroke="rgb(244, 47, 47)" strokeWidth={2} fill="none"/>
-				</g>
-			)
-		}
-
-
 		const questionsList = (
 			<ul style={{ listStyle: "none" }}>
 				<li style={{ textDecoration: "underline" }}>what makes this happen?</li>
