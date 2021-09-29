f0074d94db4e7091e024171e7f098cb2883769f0
Bryan Mehall <bryanmehall@gmail.com>
2021-09-28 20:55:09 -0600

    add getNextAction selectoe

--- dev/ducks/sim/selectors.js
+++ dev/ducks/sim/selectors.js
@@ -3,3 +3,8 @@ const getSimData = (state) => (state.sim)
 export const getLoadState = (state) => (getSimData(state).loadState)
 
 export const getKeyframes = (state) => (getSimData(state).keyframes)
+
+export const getNextAction = (state) => {
+    console.log(getSimData(state))
+    getSimData(state).onComplete
+}
